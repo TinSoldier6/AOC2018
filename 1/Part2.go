436c57051fc948fb420744d221f1b8e7a52f63e6
Jason Gade <TinSoldier6@users.noreply.github.com>
2018-12-04 11:24:28 -0800

    Add files via upload

--- 1/Part2.go
+++ 1/Part2.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <inputfile>", os.Args[0])
+		os.Exit(1)
+	}
+
+	infile, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var list []int
+	var seen = make(map[int]bool)
+	var finished bool
+	seen[0] = true
+
+	input := bufio.NewScanner(infile)
+	frequency := 0
+	for input.Scan() {
+		change, err := strconv.Atoi(input.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		frequency += change
+		list = append(list, change)
+		if seen[frequency] {
+			finished = true
+		}
+		seen[frequency] = true
+	}
+
+	for !finished {
+		for _, v := range list {
+			frequency += v
+			if seen[frequency] {
+				finished = true
+				break
+			}
+			seen[frequency] = true
+		}
+	}
+
+	fmt.Fprintln(os.Stdout, frequency)
+}
