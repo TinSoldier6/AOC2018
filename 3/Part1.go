5910a4c83a40d124ac5b0897b61d7a74c0966db7
Jason Gade <jaygade@yahoo.com>
2018-12-04 19:54:04 -0800

    In progress

--- 3/Part1.go
+++ 3/Part1.go
@@ -5,7 +5,32 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
+
+type rectangle struct {
+	id, left, top, width, height int
+}
+
+func (r1 rectangle) union(r2 rectangle) rectangle {
+	if r1.left > r2.left+r2.width || r2.left > r1.left+r1.width {
+		return rectangle{}
+	}
+	if r1.top > r2.top+r2.height || r2.top > r1.top+r1.height {
+		return rectangle{}
+	}
+	left := max(r1.left, r2.left)
+	top := max(r1.top, r2.top)
+	right := min(r1.left+r1.width, r2.left+r2.width)
+	bottom := min(r1.top+r1.height, r2.top+r2.height)
+	return rectangle{0, left, top, right - left, bottom - top}
+}
+
+func (r1 rectangle) area() int {
+	return r1.width * r1.height
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -18,6 +43,58 @@ func main() {
 		log.Fatal(err)
 	}
 
+	rectangles := make([]rectangle, 0)
 	input := bufio.NewScanner(infile)
+	for input.Scan() {
+		nextRect, err := parseRectangle(input.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		rectangles = append(rectangles, nextRect)
+	}
 
+	var overlap int
+	for i, r1 := range rectangles {
+		for _, r2 := range rectangles[i:] {
+			area := r1.union(r2).area()
+			fmt.Printf("%d ", area)
+			overlap += area
+		}
+	}
+
+	fmt.Fprintln(os.Stdout, overlap)
+
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+func parseRectangle(s string) (rect rectangle, err error) {
+	tokens := strings.FieldsFunc(s, func(c rune) bool {
+		return !unicode.IsNumber(c)
+	})
+	if len(tokens) != 5 {
+		return rectangle{}, strconv.ErrSyntax
+	}
+
+	values := make([]int, 5)
+	for i, v := range tokens {
+		values[i], err = strconv.Atoi(v)
+		if err != nil {
+			return rectangle{}, err
+		}
+	}
+
+	return rectangle{values[0], values[1], values[2], values[3], values[4]}, nil
 }
